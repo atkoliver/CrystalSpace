d155b06ce539172dad1e06ec2d335ebe4bdb8bd6
atkoliver <https://github.com/atkoliver/>
2026-01-30 17:37:01 +0100

    Added windows batch script for getting latest paper server

--- debug-scripts/get-server.bat
+++ debug-scripts/get-server.bat
@@ -1,54 +1,7 @@
-@echo on
-::TODO: Define version and dl urls for spigot, sponge, bukkit and folia
+@echo off
 
-:: Check if there are 2 arguments
+::TODO. Arguments: CraftBukkit, Spigot, Sponge, Paper, Folia
+:: Set correct URLs for finding latest build version $v & downloading server jar $d
 
-set "error=0"
-if "%~1"=="" (
-    echo [ERROR] %~nx0: Missing argument MC-VERSION!
-    echo [ERROR] %~nx0: Example: %~nx0 1.21.11 paper
-    set "error=1"
-)
-if %error% equ 1 exit /b 1
-if "%~2"=="" (
-    echo [ERROR] %~nx0: Missing argument SERVER-TYPE!
-    echo [ERROR] %~nx0: Example: %~nx0 1.21.11 paper
-    set "error=1"
-)
-if %error% equ 1 exit /b 1
-
-:: Create server folder and eula
-if not exist "server-%~1\" (
-    mkdir "server-%~1"
-    echo eula=true >> "server-%~1\eula.txt"
-    copy "server.properties.defaults" "server-%~1\server.properties"
-)
-
-::Set correct URLs for finding latest build version & downloading server jar
-
-set "error=1"
-if "%~2" == "paper" (
-    set "vurl=https://api.papermc.io/v2/projects/paper/versions/$v"
-    set "dlurl=https://api.papermc.io/v2/projects/paper/versions/$v/builds/$b/downloads/paper-$v-$b.jar"
-    set "error=0"
-)
-if "%~2" == "folia" (
-    ::set "vurl="
-    ::set "dlurl="
-    ::set "error=0"
-)
-if "%~2" == "spigot" (
-    ::set "vurl="
-    ::set "dlurl="
-    ::set "error=0"
-)
-if "%~2" == "bukkit" (
-    ::set "vurl="
-    ::set "dlurl="
-    ::set "error=0"
-)
-if %error% equ 1 echo [ERROR] %~nx0: Missing argument SERVER-TYPE!
-if %error% equ 1 exit /b 1
-
-::Script downloads latest build of server
-powershell -c "$v='%~1'; $b=(irm %vurl%).builds[-1]; $u=\"%dlurl%\"; iwr $u -OutFile server-$v/server-paper-$v.jar";
+::Downloads latest paper build
+powershell -c "$v='%~1'; $b=(irm https://api.papermc.io/v2/projects/paper/versions/$v).builds[-1]; $d=\"https://api.papermc.io/v2/projects/paper/versions/$v/builds/$b/downloads/paper-$v-$b.jar\"; iwr $d -OutFile server-$v/server-paper-$v.jar"; echo \"Downloaded latest %~1 paper build version $b\""
--- debug-scripts/start-server.bat
+++ debug-scripts/start-server.bat
@@ -3,27 +3,44 @@
 :: arg2 = SERVER-VERSION (select correct server .jar)
 
 :: Check if there are 2 arguments
-:exiterror
-echo [ERROR] %~nx0: Usage:   %~nx0 MC-VERSION SERVER-TYPE
-echo [ERROR] %~nx0: Example: %~nx0 1.21.11 paper
-exit /b 1
-
+set "error=0"
 if "%~1"=="" (
     echo [ERROR] %~nx0: Missing argument MC-VERSION!
-    call :exiterror
+    echo [ERROR] %~nx0: Example: %~nx0 1.21.11 paper
+    set "error=1"
 )
+if %error% == 1 ( exit /b 1 )
 if "%~2"=="" (
     echo [ERROR] %~nx0: Missing argument SERVER-TYPE!
-    call :exiterror
+    echo [ERROR] %~nx0: Example: %~nx0 1.21.11 paper
+    set "error=1"
+)
+if %error% == 1 ( exit /b 1 )
+
+:: Check if folder exists
+if not exist "server-%~1\" (
+    mkdir "server-%~1"
+    echo eula=true >> "server-%~1\eula.txt"
+    copy "server.properties.defaults" "server-%~1\server.properties"
+)
+:: Check if jar exists.
+if not exist "server-%~1\server-%~2-%~1.jar" (
+    ::If not, download server
+    cmd /K get-server.bat %1 %2
 )
 
-:: Copy latest plugin build
+cd server-%~1\
 
-copy /Y ..\build\libs\CrystalSpace-%1.jar server\plugins\CrystalSpaceTestBuild.jar
+:: Copy latest plugin build
+copy /Y ..\..\build\libs\CrystalSpace-%1.jar plugins\CrystalSpaceTestBuild.jar
 
 :: arg1 = Open up server for remote debugger	= -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
 :: arg2 = Optimization for server		= -XX+Always [...] etc  
 
-cd server\
+:: Launch server
+:: -agentlib: Opens up port 5005 for debugger attacher to connect
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar server-%~2-%~1.jar nogui
 
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -jar server-%~2-%~1.jar nogui
+:: These are extra server arguments recommended by papermc.io
+:: TODO: Test if they reduce server start-up time. Otherwise, remove this
+:: -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32
