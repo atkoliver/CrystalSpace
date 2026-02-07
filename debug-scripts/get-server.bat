d6f89b060ad1c4a6b2aaf194f9d8b03e1b65c570
atkoliver <github.com/atkoliver>
2026-02-07 23:57:41 +0100

    Reapply "Added Linux debug scripts! Also made get-server.bat readable"
    This reverts commit 9010285ba16e4c1a32093fc9a5b6bfb7b7178fa0.

--- debug-scripts/get-server.bat
+++ debug-scripts/get-server.bat
@@ -1,7 +1,22 @@
 @echo off
+:: arg1 = MC-VERSION (select correct server.jar + select correct CrystalSpace test build)
+:: arg2 = SERVER-VERSION (select correct server .jar)
 
-::TODO. Arguments: CraftBukkit, Spigot, Sponge, Paper, Folia
-:: Set correct URLs for finding latest build version $v & downloading server jar $d
+::TODO. Use Server-Verion Argument: CraftBukkit, Spigot, Sponge, Paper, Folia.
+::      Set correct URLs for finding latest build version $ver & downloading server jar $d
 
 ::Downloads latest paper build
-powershell -c "$v='%~1'; $b=(irm https://api.papermc.io/v2/projects/paper/versions/$v).builds[-1]; $d=\"https://api.papermc.io/v2/projects/paper/versions/$v/builds/$b/downloads/paper-$v-$b.jar\"; iwr $d -OutFile server-$v/server-paper-$v.jar"; echo \"Downloaded latest %~1 paper build version $b\""
+set commands=$verer=1'%~1'; ^
+$build=(irm https://api.papermc.io/v2/projects/paper/versions/$ver).builds[-1]; ^
+$url="https://api.papermc.io/v2/projects/paper/versions/$ver/builds/$build/downloads/paper-$ver-$build.jar\"; ^
+iwr $url -OutFile "./server-$ver/server-paper-$ver.jar"; ^
+if (Test-Path -LiteralPath "./server-$ver/server-paper-$ver.jar") ^
+{ ^
+  echo "Downloaded latest %~1 paper build version $build"; ^
+} ^
+else ^
+{ ^
+  echo "[ERROR] %~nx0: Couldn't download file."; ^
+}
+
+powershell -c %commands%
